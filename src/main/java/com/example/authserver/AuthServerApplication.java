7f0d4775f9671d66d1d946ad3f66895c1da368ac
“Deepak” <109643803+deepnz@users.noreply.github.com>
2023-12-04 09:54:52 -0600

    yaml file modf

--- src/main/java/com/example/authserver/AuthServerApplication.java
+++ src/main/java/com/example/authserver/AuthServerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
@@ -12,7 +13,6 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 public class AuthServerApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(AuthServerApplication.class, args);
 	}
 
@@ -20,8 +20,8 @@ public class AuthServerApplication {
 	UserDetailsService inMemoryUserDetailsManager() {
 		var userBuilder = User.builder();
 		return new InMemoryUserDetailsManager(
-				userBuilder.roles("USER").username("deepak").password("{bcrypt}$2a$10$O3C2zBwYsU94RJhyCBE1xuh97JlBfndTnEcU/3i1MOI8CLGx8Qmzy").build(),
-				userBuilder.roles("USER","ADMIN").username("dagod").password("{bcrypt}$2a$10$amBbkdtq6Efbqek8u/Dfou9.Ws.tbbJgESMnDR57OvZ3Xme7kzCmu").build()
+				userBuilder.roles("USER").username("deepak").password("{bcrypt}$2a$10$h0JjKkXogaQTGlUwQb7RcOAe4.J6/d9WuJyqRiQxE27stv6icQwz2").build(),
+				userBuilder.roles("USER","ADMIN").username("admin").password("{bcrypt}$2a$10$OWmNC3FmLftgkST51PZ5LOj04NgPONkah3Pik0b/v7CWvJcdIBI/O").build()
 		);
 	}
 	@Bean
