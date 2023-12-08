c00d070d089a02e93429ea87409a7dd14caaa733
“Deepak” <109643803+deepnz@users.noreply.github.com>
2023-12-08 09:50:42 -0600

    added postgres jdbc

--- src/main/java/com/example/authserver/AuthServerApplication.java
+++ src/main/java/com/example/authserver/AuthServerApplication.java
@@ -1,13 +1,25 @@
 package com.example.authserver;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.Set;
+import java.util.UUID;
 
 @SpringBootApplication
 public class AuthServerApplication {
@@ -30,5 +42,34 @@ public class AuthServerApplication {
 				.build();
 	}
 }
+@Configuration
+class ClientConfiguration{
+	@Bean
+	RegisteredClientRepository registeredClientRepository(JdbcTemplate template) {
+		return new JdbcRegisteredClientRepository(template);
+	}
 
+	@Bean
+	ApplicationRunner clientsRunner(RegisteredClientRepository repository) {
+		return args -> {
+			var clientId = "app";
+			if (repository.findByClientId(clientId) == null) {
+				repository.save(
+						RegisteredClient
+								.withId(UUID.randomUUID().toString())
+								.clientId(clientId)
+								.clientSecret("{bcrypt}$2a$10$2.v1HhI.JGQuzC5/Uc0oFud82ufoLkH93ER8PvGmjKCWp1x41FooC")
+								.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+								.authorizationGrantTypes(grantTypes -> grantTypes.addAll(Set.of(
+										AuthorizationGrantType.CLIENT_CREDENTIALS,
+										AuthorizationGrantType.AUTHORIZATION_CODE,
+										AuthorizationGrantType.REFRESH_TOKEN)))
+								.redirectUri("http://127.0.0.1:8282/login/oauth2/code/spring")
+								.scopes(scopes -> scopes.addAll(Set.of("user.read", "user.write", OidcScopes.OPENID)))
+								.build()
+				);
+			}
+		};
+	}
+}
 	
