e643bed1852b6fb74ef333365edb0841405b3949
“Deepak” <109643803+deepnz@users.noreply.github.com>
2023-12-30 00:07:54 -0600

    added keys just to test, removed from version control

--- src/main/java/com/example/authserver/AuthServerApplication.java
+++ src/main/java/com/example/authserver/AuthServerApplication.java
@@ -1,5 +1,10 @@
 package com.example.authserver;
 
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -25,6 +30,14 @@ import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 
 import javax.sql.DataSource;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
@@ -42,6 +55,23 @@ public class AuthServerApplication {
 				.build();
 	}
 }
+
+ 	@Configuration
+	class KeysConfiguration {
+
+	@Bean
+		JWKSource<SecurityContext> jwkSource(
+			@Value("${jwt.key.id}") String id,
+			@Value("${jwt.key.private}") RSAPrivateKey privateKey,
+			@Value("${jwt.key.public}") RSAPublicKey publicKey) {
+		 var rsa = new RSAKey.Builder(publicKey)
+				 .privateKey(privateKey)
+				 .keyID(id)
+				 .build();
+		 var jwk = new JWKSet(rsa);
+		 return new ImmutableJWKSet<>(jwk);
+		}
+	}
 	@Configuration
 	class AuthorizationConfiguration{
 
